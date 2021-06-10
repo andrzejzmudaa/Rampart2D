887741cab170ec6656e939c4ba03f5f519caa396
Andrzej <sss@wp.pl>
2021-06-10 19:22:58 +0200

    migration to v2020

--- Assets/cannonBallScript.cs
+++ Assets/cannonBallScript.cs
@@ -13,46 +13,35 @@ public class cannonBallScript : MonoBehaviour
 	[Tooltip("How high the arc should be, in units")]
 	public float arcHeight = 1;
 
+	[Tooltip("Horizontal speed, in units/sec")]
+	public float minSpeed = 10f;
+
 	bool allowMomvement;
 	Vector3 startPos;
 	Vector3 nextPos;
 	float shootAngle;
-
+	float tempspeed;
+	float x0;
+	float x1;
+	float dist;
 	void Start()
 	{
 		// Cache our start position, which is really the only thing we need
 		// (in addition to our current position, and the target).
 		startPos = transform.position;
-		shootAngle = transform.eulerAngles.z;
-		Debug.Log("shootAngles: " + Mathf.Cos(shootAngle * Mathf.Deg2Rad));
-	}
+		tempspeed = Mathf.Abs(Mathf.Sin(shootAngle * Mathf.Deg2Rad) * speed);
+		x0 = startPos.x;
+		x1 = targetPos.x;
+		dist = x1 - x0;
 
+	}
 	void Update()
 	{
-		if (!allowMomvement)
-			return;
-		// Compute the next position, with arc added in
-		float tempspeed = Mathf.Abs(Mathf.Sin(shootAngle * Mathf.Deg2Rad) * speed);
-		float x0 = startPos.x;
-		float x1 = targetPos.x;
-		float dist = x1 - x0;
-		float nextX = Mathf.MoveTowards(transform.position.x, x1, tempspeed * Time.deltaTime);
-		float baseY = Mathf.Lerp(startPos.y, targetPos.y, (nextX - x0) / dist);
-		float arc = arcHeight * (nextX - x0) * (nextX - x1) / (-0.25f * dist * dist);
-		nextPos = new Vector3(nextX, baseY + arc, transform.position.z);
-
-		// Rotate to face the next position, and then move there
-		transform.rotation = LookAt2D(nextPos - transform.position);
-		transform.position = nextPos;
-
-		//Debug.Log("ActualPos: " + transform.position);
-		//Debug.Log("TargetPos: " + targetPos);
-		//Debug.Log("Wynik:1 " + comparePosVector(transform.position, targetPos));
-		// Do something when we reach the target
-		if (comparePosVector(transform.position, targetPos)) 
-			Arrived();
+		if (Mathf.Abs(dist) > 0.5f)
+			arcMovement();
+		else
+			straightMovement();
 	}
-
 	void Arrived()
 	{
 		Destroy(this.gameObject);
@@ -69,16 +58,45 @@ public class cannonBallScript : MonoBehaviour
 		return Quaternion.Euler(0, 0, Mathf.Atan2(forward.y, forward.x) * Mathf.Rad2Deg);
 	}
 
-	public void setTargetPos(Vector3 _targetPos)
-    {
+	public void setTargetPos(Vector3 _targetPos, float _shootAngle)
+	{
 		targetPos = _targetPos;
 		allowMomvement = true;
-
+		shootAngle = _shootAngle;
 	}
 
-	bool comparePosVector(Vector3 _vector1 , Vector3 _vector2)
-    {
+	bool comparePosVector(Vector3 _vector1, Vector3 _vector2)
+	{
 		return Vector2.SqrMagnitude(new Vector2(_vector1.x, _vector1.y) - new Vector2(_vector2.x, _vector2.y)) < 0.0001;
 
 	}
+	void arcMovement()
+	{
+		if (!allowMomvement)
+			return;
+		// Compute the next position, with arc added in
+
+		float nextX = Mathf.MoveTowards(transform.position.x, x1, tempspeed * Time.deltaTime);
+		float baseY = Mathf.Lerp(startPos.y, targetPos.y, (nextX - x0) / dist);
+		float arc = arcHeight * (nextX - x0) * (nextX - x1) / (-0.25f * dist * dist);
+		nextPos = new Vector3(nextX, baseY + arc, transform.position.z);
+
+		// Rotate to face the next position, and then move there
+		transform.rotation = LookAt2D(nextPos - transform.position);
+		transform.position = nextPos;
+
+		// Do something when we reach the target
+		if (comparePosVector(transform.position, targetPos))
+			Arrived();
+		Debug.Log("Arc movement " + dist);
+	}
+
+
+
+	void straightMovement()
+	{
+		Destroy(this.gameObject);
+
+	}
 }
+
--- Assets/cannon_prefab_script.cs
+++ Assets/cannon_prefab_script.cs
@@ -7,6 +7,9 @@ public class cannon_prefab_script : MonoBehaviour
     int healthPoints;
     public cannonBallScript cannonBall;
     cannonBallScript cannonBallSpawned;
+    float cannonAngle;
+    float xBallStartMove;
+    float yBallStartMove;
 
     public int HealthPoints { get => healthPoints; }
     // Start is called before the first frame update
@@ -23,14 +26,47 @@ public class cannon_prefab_script : MonoBehaviour
 
     public void rotateToPlayerAim(Vector3 _vectorTo)
     {
-        float angle = Quaternion.FromToRotation(Vector3.up, _vectorTo - this.gameObject.transform.position).eulerAngles.z;
-        this.transform.eulerAngles = new Vector3(0,0, angle);
+        cannonAngle = Quaternion.FromToRotation(Vector3.up, _vectorTo - this.gameObject.transform.position).eulerAngles.z;
+        if (cannonAngle >= 0 && cannonAngle < 160)
+        {
+            this.transform.eulerAngles = new Vector3(0, 0, 45);
+            xBallStartMove = -0.75f;
+            yBallStartMove = 0.75f;
+        }
+        else if (cannonAngle >= 160 && cannonAngle < 165)
+        {
+            this.transform.eulerAngles = new Vector3(0, 0, 90);
+            xBallStartMove = -1f;
+            yBallStartMove = 0f;
+        }
+        else if (cannonAngle >= 165 && cannonAngle < 170)
+        {
+            this.transform.eulerAngles = new Vector3(0, 0, 45);
+            xBallStartMove = -0.75f;
+            yBallStartMove = 0.75f;
+        }
+        else if (cannonAngle >= 170 && cannonAngle < 190)
+        {
+            this.transform.eulerAngles = new Vector3(0, 0, 0);
+            xBallStartMove = 0f;
+            yBallStartMove = 0.8f;
+        }
+        else if (cannonAngle >= 190 && cannonAngle < 195)
+        {
+            this.transform.eulerAngles = new Vector3(0, 0, 0);
+            xBallStartMove = 0.75f;
+            yBallStartMove = 0.75f;
+        }
+
+
+
+        //this.transform.eulerAngles = new Vector3(0, 0, angle);
     }
 
     public void shootCanonBall(Vector3 _targetPos)
     {
-        cannonBallSpawned = Object.Instantiate<cannonBallScript>(cannonBall, this.transform.position, this.transform.rotation , this.gameObject.transform.parent);
-        cannonBallSpawned.setTargetPos(_targetPos);
+        cannonBallSpawned = Object.Instantiate<cannonBallScript>(cannonBall, new Vector3(this.transform.position.x + xBallStartMove, this.transform.position.y + yBallStartMove, -2), this.transform.rotation , this.gameObject.transform.parent);
+        cannonBallSpawned.setTargetPos(_targetPos , cannonAngle);
 
 
     }
